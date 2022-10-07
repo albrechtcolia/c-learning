1499635367fd31dcd15b42ad7812762b3af7b76f
Marcus Flaminius Rufus <107230982+albrechtcolia@users.noreply.github.com>
2022-10-07 20:53:18 -0300

    Add files via upload

---
+++ 01-character_declaration.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char ch1 = 'A';
+    char ch2 = 65;
+    int ch3 = '1';
+    int ch4 = 49;
+    int ch5;
+
+    printf("ch1, ch2\n");
+    printf("\tchar: %c %c\n", ch1, ch2);
+    printf("\tint.: %d %d\n\n", ch1, ch2);
+
+    printf("ch3, ch4\n");
+    printf("\tchar: %c %c\n", ch3, ch4);
+    printf("\tint.: %d %d\n", ch3, ch4);
+
+    printf("ch5\n");
+    ch5 = 'M';
+    printf("\tch5: %c %d\n", ch5, ch5);
+
+    ch5 = 74;
+    printf("\tch5: %c %d\n", ch5, ch5);
+
+    return 0;
+}
