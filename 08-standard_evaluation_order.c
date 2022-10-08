59a00477972db5a0127ef4c69befab408ecf8936
Marcus Flaminius Rufus <107230982+albrechtcolia@users.noreply.github.com>
2022-10-08 02:57:57 -0300

    Add files via upload

---
+++ 08-standard_evaluation_order.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // Example 1.
+    char ch    = 'a';
+    char str[] = "Iesus";
+
+    if ('a' == ch)
+        printf("Option 1\n");
+
+    if (str[0] == 'I' &&
+        str[1] == 'e' &&
+        str[2] == 's' &&
+        str[3] == 'u' &&
+        str[4] == 's')
+
+        printf("Option 2\n");
+
+    // Example 2.
+    int a = 0;
+
+    if (str[a++] == 'I')
+        printf("Option 3\n");
+
+    a = 0;
+    if (str[++a] == 'e')
+        printf("Option 4\n");
+
+    return 0;
+}
