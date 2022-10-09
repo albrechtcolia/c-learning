20ad5687390a5c5d77bd0e7c607ce1d37c4cd9ac
Marcus Flaminius Rufus <107230982+albrechtcolia@users.noreply.github.com>
2022-10-09 00:09:36 -0300

    Add files via upload

---
+++ 09-loop_basic.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+   /**
+    *   Loop - basic form.
+    *   
+    *   Statement 1 - initialization
+    *       Is executed one time and before
+    *       the execution of the code block.
+    * 
+    *   Statement 2 - condition
+    *       Defines the condition for executing
+    *       the code block.
+    * 
+    *   Statement 3 - increment/decrement
+    *       Is executed every time after the
+    *       code block has been executed..
+    */
+    
+    int a;
+
+    for (a = 0; a == 0 || a == 1; a++)
+        printf("Value: %d\n", a);
+
+    printf("Value: %d\n", a);
+
+    return 0;
+}
