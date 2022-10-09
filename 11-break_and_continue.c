642b19503a099135ebe23f63dc474d1156462dff
Marcus Flaminius Rufus <107230982+albrechtcolia@users.noreply.github.com>
2022-10-09 00:10:01 -0300

    Add files via upload

---
+++ 11-break_and_continue.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    /**
+     * Break example.
+     *  Interrupts the execution of the loop,
+     *  ignoring all following code.
+     */
+
+    for (int a = 0; a < 10; a++) {
+        printf("1 - Value: %d\n", a);
+
+        if (a == 5) {
+            printf("Finalizando o loop.\n");
+            break;
+        }
+    }
+
+    printf("\n");
+
+    /**
+     * Continue example.
+     *  It does not stop the execution of
+     *  the loop, but ignores all following code.
+     */
+
+    for (int a = 0; a < 10; a++) {
+        if (a == 5) {
+            printf("Pula o 5.\n");
+            continue;
+        }
+
+        printf("2 - Value: %d\n", a);
+    }
+
+    return 0;
+}
