2d6bad280630ccb44e0880a76719df670878de72
Marcus Flaminius Rufus <107230982+albrechtcolia@users.noreply.github.com>
2022-10-11 12:16:51 -0300

    Add files via upload

---
+++ 15-copying_strings.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * Copy a string into another string.
+ */
+
+int main (int argc, char *argv[])
+{
+    char str1[] = "Iesus Hominum Salvator";
+    // Definition goes from 1 to 100.
+    char str2[100];
+
+    // Access goes from 0 to 99.
+    for (int a = 0; a < 100; a++)
+        str2[a] = '\0';
+
+    for (int a = 0; str1[a] != '\0'; a++)
+        str2[a] = str1[a];
+
+    printf("%s\n",str2);
+
+    return 0;
+}
---
+++ 16-concatenate_two_strings.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * Concatenate two strings.
+ */
+
+int main (int argc, char *argv[])
+{
+    char str1 [100];
+    char str2 [] = "Iesus";
+    char str3 [] = " Salvator";
+    int size;
+
+    // Copy str2 to str1.
+    for (size = 0; str2[size] != '\0'; size++)
+        str1[size] = str2[size];
+
+    str1[size] = '\0';
+
+    printf("%s\n", str1);
+    
+    // Concatenate str3 to str1.
+    for (size = 0; str1[size] != '\0'; size++);
+
+    for (int a = 0; str3[a] != '\0'; a++, size++)
+        str1[size] = str3[a];
+    
+    str1[size] = '\0';
+
+    printf("%s\n", str1);
+
+    return 0;
+}
