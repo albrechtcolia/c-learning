385c1b2bb41bfd638f0cbc371e6b8a58c23f9133
Marcus Flaminius Rufus <107230982+albrechtcolia@users.noreply.github.com>
2022-10-10 22:41:37 -0300

    Add files via upload

---
+++ 12-character_handling.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    char str[] = "Iesus";
+    int index;
+
+    // Prints every character of my string.
+    for (index = 0; str[index] != '\0'; index++)
+        printf("%c ", str[index]);
+
+    printf("\n");
+
+    printf("Before: %s\n", str);
+
+    // Defines each character of my string as '\0'.
+    for (index = 0; str[index] != '\0'; index++)
+        str[index] = '\0';
+
+    printf("After: %s\n", str);
+
+    return 0;
+}
---
+++ 13-character_count.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    char str[] = "Iesus";
+    int index;
+    
+    // Counts each character of my string including the '\0'.
+    for (index = 0; str[index] != '\0'; index++);
+
+    printf("str size: %d\n", index);
+
+    // Counts each character of my string without including the '\0'.
+    for (index = -1; str[index+1] != '\0'; index++);
+
+    printf("str size: %d\n", index);
+
+    return 0;
+}
