e3b3412ff49ed645e588538c201e935d30103c02
Marcus Flaminius Rufus <107230982+albrechtcolia@users.noreply.github.com>
2022-10-11 12:47:19 -0300

    Add files via upload

---
+++ 18-occurrence_finder.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * Find occurrence in a string.
+ */
+
+int main (int argc, char *argv[])
+{
+    char str1[] = "Iesus Hominum Salvator";
+    char str2[] = "Hominum";
+    int flag = 0;
+
+    for (int a = 0; str1[a] != '\0'; a++) {
+        for (int b = 0; str2[b] != '\0'; b++) {
+            if (str2[b] == str1[a + b])
+                flag = 1;
+            else {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag)
+            break;
+    }
+
+    // Checking.
+    if (flag)
+        printf("Found the occurrence.\n");
+    else
+        printf("Did not find the occurrence.\n");
+
+    return 0;
+}
---
+++ 19-split_string.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+
+/**
+ * Split a string into two.
+ */
+
+int main (int argc, char *argv[])
+{
+    char str1[] = "Jesus Christ";
+    char str2[100], str3[100];
+    int size;
+
+    // Reset 'str2' and 'str3'.
+    for (int a = 0; a < 100; a++) {
+        str2[a] = '\0';
+        str3[a] = '\0';
+    }
+
+    // Get the size of 'str1' without the '\0.
+    for (size = -1; str1[size+1] != '\0'; size++);
+
+    // Splits str1 into two strings.
+    for (int a = 0, b = 0; str1[a] != '\0'; a++) {
+        if (a <= (size/2))
+            str2[a] = str1[a];
+
+        else {
+            str3[b] = str1[a];
+            b++;
+        }
+    }
+
+    printf("str1: %s\n", str1);
+    printf("str2: %s\n", str2);
+    printf("str3: %s\n", str3);
+
+    return 0;
+}
+
+
