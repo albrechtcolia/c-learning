5eaf2cea10376d43b1663de5d34f4bb5b11626c3
Marcus Flaminius Rufus <107230982+albrechtcolia@users.noreply.github.com>
2022-10-11 13:22:22 -0300

    Add files via upload

---
+++ 20-splitting_string_in_tokens.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+
+/**
+ * Splitting a string into tokens.
+ */
+
+int main (int argc, char *argv[])
+{
+    char str[] = "In principio erat Verbum";
+    char ocr   = ' ';
+    char str2[50];
+
+    for (int a = 0, b = 0; ; a++) {
+        if (str[a] != ocr && str[a] != '\0') {
+            str2[b] = str[a];
+            b++;
+        }
+
+        else {
+            if (b) {
+                str2[b] = '\0';
+                b = 0;    
+                
+                printf("> %s\n", str2);
+            }
+        }
+
+        if (str[a] == '\0')
+            break;
+    }
+
+    return 0;
+}
+
+
