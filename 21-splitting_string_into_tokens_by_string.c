60f438df55a6cb5ef96f4a6f539ea73c4d1f5345
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-13 23:50:47 -0300

    updates

---
+++ 21-splitting_string_into_tokens_by_string.c
@@ -0,0 +1,48 @@
+
+#include <stdio.h>
+
+/**
+ * Splitting a string into tokens,
+ * when the token is a string.
+ */
+
+int main (int argc, char *argv[])
+{
+    char str    [] = "+++In+++principio+++erat+++Verbum";
+    char ocr    [] = "+++";
+    char token  [100];
+
+    for (int a = 0, b = 0, c = 0, flag = 0; ; a++) {
+        for (b = 0; ocr[b] != '\0'; b++) {
+            if (ocr[b] == str[a + b])
+                flag = 1;
+            else {
+                flag = 0;
+                break;
+            }
+        }
+
+        if (flag || str[a] == '\0') {
+            if (c) {
+                token[c] = '\0';
+                c = 0;
+            
+                printf("token: %s\n", token);
+            }
+
+            if (str[a] == '\0')
+                break;
+
+            a += (b-1);
+        }
+
+        else {
+            token[c] = str[a];
+            c++;
+        }
+    }
+
+    return 0;
+}
+
+
