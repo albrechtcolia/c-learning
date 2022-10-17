9b93089053b8df128a9778b711ecf923cd6ec512
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-16 23:31:14 -0300

    updates

---
+++ 28-struct_initialization.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <string.h>
+
+struct data {
+    int  age;
+    char name[100];
+};
+
+int main (int argc, char *argv[])
+{
+    // Initialize all members.
+    struct data dt1 = { 
+        .age  = 10,
+        .name = "Iesus"
+    };
+
+    // Does not initialize all members.
+    struct data dt2 = {
+        .age  = 20
+    };
+
+    memset(dt2.name, '\0', 100);
+
+    printf("1 -> %d, %s\n", dt1.age, dt1.name);
+    printf("2 -> %d, %s\n", dt2.age, dt2.name);
+
+    return 0;
+}
+
+
---
+++ 29-struct_typedef.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <string.h>
+
+struct data_s {
+    int age;
+    char name [100];
+};
+
+typedef struct data_s data_t;
+
+int main (int argc, char *argv[])
+{
+    data_t data = {
+        .age = 0, 
+        .name = "Iesus"
+    };
+
+    printf("data: %d, %s\n", data.age, data.name);
+
+    return 0;
+}
+
+
