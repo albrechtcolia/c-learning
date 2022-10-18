ee9701dc4ee424219f27e41db3ffd0500e8b3220
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-18 11:33:45 -0300

    updates

---
+++ 34-basic_string_functions.c
@@ -0,0 +1,46 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Declaration.
+void func1 (char *str)
+{
+    if (!str)
+        return;
+
+    printf("str: %s\n", str);
+}
+
+int main (int argc, char *argv[])
+{
+    // Function calls.
+    char str1[] = "Test 1";
+
+    func1(str1);
+    func1("Test 2");
+
+    // Using pointers.
+    char *str2;
+    int size;
+
+    size = sizeof(char) * 100;
+    str2 = (char *) malloc(size);
+
+    if (!str2) {
+        printf("Malloc failed.\n");
+        exit(-1);
+    }
+
+    memset(str2, '\0', size);
+    sprintf(str2, "Test 3");
+
+    func1(str2);
+
+    memset(str2, '\0', size);
+    free(str2);
+
+    return 0;
+}
+
+
---
+++ 35-string_return_functions.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Returning a character.
+char func1 ()
+{
+    return 'a';
+}
+
+// Returning a dynamically allocated string.
+char *func2 ()
+{
+    int sz = sizeof(char) * 100;
+    char *s = (char *) malloc(sz);
+
+    if (!s)
+        return NULL;
+    
+    memset(s, '\0', sz);
+    sprintf(s, "Iesus Salvator");
+
+    return s;
+}
+
+int main (int argc, char *argv[])
+{
+    // Returning a character.
+    int ch = func1();
+
+    printf("ch: %c\n", ch);
+
+    // Returning a dynamically allocated string.
+    char *string = func2();
+
+    if (!string) {
+        printf("malloc failed.\n");
+
+        exit(-1);
+    }
+
+    printf("string: %s\n", string);
+        
+    memset(string, '\0', strlen(string)); 
+    free(string);
+
+    return 0;
+}
+
+
