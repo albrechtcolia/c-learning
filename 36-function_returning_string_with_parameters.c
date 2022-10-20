4616c1fe2a01da618247ff9c45473e662b57713c
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-19 22:07:42 -0300

    updates

---
+++ 36-function_returning_string_with_parameters.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *func1 (char *param)
+{
+    int sz  = sizeof(char) * 100;
+    char *s = (char *) malloc(sz);
+
+    if (!s)
+        return NULL;
+    
+    memset(s, '\0', sz);
+    sprintf(s, "Iesus %s", param);
+ 
+    return s;
+}
+
+int main (int argc, char *argv[])
+{
+    char *str = func1("Salvator <3");
+
+    if (str) {
+        printf("func1(): %s\n", str);
+        
+        memset(str, '\0', strlen(str));
+        free(str);
+    }
+
+    return 0;
+}
+
+
