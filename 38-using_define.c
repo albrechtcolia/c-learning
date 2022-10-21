161cd92424b71a28d25441919ea3e35cfacb3b2c
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-20 23:07:59 -0300

    updates

---
+++ 38-using_define.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+// Replaces in all occurrences found.
+#define VALOR_GLOBAL 10
+
+int main (int argc, char *argv[]) {
+    int a = VALOR_GLOBAL;
+
+    printf("valor: %d\n", a);
+
+    return 0;
+}
+
+
