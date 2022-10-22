346b8649d67fe72b8a29b21473d725481f579c79
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-21 23:56:48 -0300

    updates

---
+++ 40-define_parameters.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+#define calc(P1, P2) printf("Value: %d\n", (P1 * P2));
+
+int main (int argc, char *argv[]) {
+    int a = 10, b = 20;
+
+    calc(a, b)
+
+    return 0;
+}
+
+
