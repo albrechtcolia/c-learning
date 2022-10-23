2b89b15f08a37ee4b7d6679d3e26d9f3b5883cbe
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-22 21:00:11 -0300

    updates

---
+++ 41-define_multiple_parameters.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+#define my_printf(FMT, ...) printf(FMT, ##__VA_ARGS__);
+
+int main (int argc, char *argv[]) {
+    int a = 10, b = 20;
+
+    my_printf("Valor: %d + %d = %d\n", a, b, (a + b))
+
+    return 0;
+}
+
+
