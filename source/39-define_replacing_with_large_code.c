a8391031ee42dbd046061b31a41af3dbf84e3ae6
Albrechtcolia <albrechtcolia@gmail.com>
2023-02-11 21:31:19 -0300

    updates

--- source/39-define_replacing_with_large_code.c
+++ source/39-define_replacing_with_large_code.c
@@ -4,9 +4,9 @@
 #define PRINT_ALL_VARIABLES         \
     int c = a + b;                  \
                                     \
-    printf("Valor 1: %d\n", a);     \
-    printf("Valor 2: %d\n", b);     \
-    printf("Valor 3: %d\n", c);
+    printf("Value 1: %d\n", a);     \
+    printf("Value 2: %d\n", b);     \
+    printf("Value 3: %d\n", c);
 
 int main (int argc, char *argv[]) {
     int a = 10, b = 20;
