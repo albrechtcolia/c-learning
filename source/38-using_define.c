71308675fa8254ecc6c8542434f650ccb41f1765
Albrechtcolia <albrechtcolia@gmail.com>
2023-02-19 23:52:47 -0300

    updates

--- source/38-using_define.c
+++ source/38-using_define.c
@@ -4,7 +4,8 @@
 // Replaces in all occurrences found.
 #define GLOBAL_VALUE 10
 
-int main (int argc, char *argv[]) {
+int main (int argc, char *argv[])
+{
     int a = GLOBAL_VALUE;
 
     printf("Value: %d\n", a);
