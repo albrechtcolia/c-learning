52cf1b57d7cd5252afa002390a64e248df4a4a42
Albrechtcolia <albrechtcolia@gmail.com>
2023-03-09 23:28:01 -0300

    updates

--- source/24-sizeof.c
+++ source/24-sizeof.c
@@ -4,11 +4,11 @@
 int main (int argc, char *argv[])
 {
     // Returns the size of a data type.
-    printf("char..: %ld bytes\n", sizeof(char));    
-    printf("int...: %ld bytes\n", sizeof(int));    
-    printf("long..: %ld bytes\n", sizeof(long));    
-    printf("float.: %ld bytes\n", sizeof(float));    
-    printf("double: %ld bytes\n", sizeof(double));    
+    printf("char..: %ld bytes\n", sizeof(char));
+    printf("int...: %ld bytes\n", sizeof(int));
+    printf("long..: %ld bytes\n", sizeof(long));
+    printf("float.: %ld bytes\n", sizeof(float));
+    printf("double: %ld bytes\n", sizeof(double));
 
     return 0;
 }
