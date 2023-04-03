8cee5fc16a9640b2377c2358d711d54cc566b09c
Albrechtcolia <albrechtcolia@gmail.com>
2023-04-02 23:48:32 -0300

    updates

--- source/25-basic_memory_allocation.c
+++ source/25-basic_memory_allocation.c
@@ -8,7 +8,7 @@ int main (int argc, char *argv[])
     char   *ptr  = (char *) malloc(size);
 
     if (!ptr) {
-        printf("malloc failed.\n");
+        printf("Malloc failed.\n");
         exit(-1);
     }
 
