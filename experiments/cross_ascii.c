acbb148c2ce8516cddf691beafee30f6d031023f
Albrechtcolia <albrechtcolia@gmail.com>
2023-04-14 22:42:09 -0300

    updates

--- experiments/cross_ascii.c
+++ experiments/cross_ascii.c
@@ -60,7 +60,7 @@ int main (int argc, char *argv[]) {
                     string[b] = '|';
                     continue;
                 }
-                
+
                 string[b] = ' ';
             }
 
