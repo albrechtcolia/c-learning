7f6e98efdb1f543277d15330d97ed294a2ad0981
Albrechtcolia <albrechtcolia@gmail.com>
2023-05-23 16:35:41 -0300

    updates

--- source/16-concatenate_two_strings.c
+++ source/16-concatenate_two_strings.c
@@ -7,12 +7,12 @@
 
 int main (int argc, char *argv[])
 {
-    char str1 [100];
-    char str2 [] = "Iesus";
-    char str3 [] = " Salvator";
+    char str1[100];
+    char str2[] = "Iesus";
+    char str3[] = " Salvator";
     int  size;
 
-    // Copy str2 to str1.
+    // Copy 'str2' to 'str1'.
     for (size = 0; str2[size] != '\0'; size++)
         str1[size] = str2[size];
 
@@ -20,7 +20,7 @@ int main (int argc, char *argv[])
 
     printf("%s\n", str1);
 
-    // Concatenate str3 to str1.
+    // Concatenate 'str3' to 'str1'.
     for (size = 0; str1[size] != '\0'; size++);
 
     for (int a = 0; str3[a] != '\0'; a++, size++)
