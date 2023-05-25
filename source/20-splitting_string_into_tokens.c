23014cbdc810070dc5479d7abbf9309d58c802b8
Albrechtcolia <albrechtcolia@gmail.com>
2023-05-25 19:32:48 -0300

    updates

--- source/20-splitting_string_into_tokens.c
+++ source/20-splitting_string_into_tokens.c
@@ -8,21 +8,21 @@
 int main (int argc, char *argv[])
 {
     char str[] = "In principio erat Verbum";
-    char ocr   = ' ';
-    char str2[50];
+    char occr  = ' ';
+    char token[50];
 
     for (int a = 0, b = 0; ; a++) {
-        if (str[a] != ocr && str[a] != '\0') {
-            str2[b] = str[a];
+        if (str[a] != occr && str[a] != '\0') {
+            token[b] = str[a];
             b++;
         }
 
         else {
             if (b) {
-                str2[b] = '\0';
+                token[b] = '\0';
                 b = 0;
 
-                printf("> %s\n", str2);
+                printf("> %s\n", token);
             }
         }
 
